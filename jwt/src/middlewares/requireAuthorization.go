c1517f618dbc6d32a8178477536915a3c42069aa
Laszlo Kohl <laszlo.jobs@gmail.com>
2025-05-11 20:34:28 -0400

    remove obsolete code

--- jwt/src/middlewares/requireAuthorization.go
+++ jwt/src/middlewares/requireAuthorization.go
@@ -44,38 +44,4 @@ func RequireAuthorization(c *gin.Context, requiredAccessLevel string) {
 	}
 	c.AbortWithStatus(http.StatusForbidden)
 
-	// return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	// 	// Retrieve the authentication cookie
-	// 	cookie, err := r.Cookie("auth_token")
-	// 	if err != nil {
-	// 		http.Error(w, "authentication cookie not found", http.StatusUnauthorized)
-	// 		return
-	// 	}
-
-	// 	// Validate the token
-	// 	token, err := middleware.ValidateToken(cookie.Value)
-	// 	if err != nil {
-	// 		http.Error(w, "invalid token", http.StatusUnauthorized)
-	// 		return
-	// 	}
-
-	// 	// Check if the token has the "access_levels" claim
-	// 	if claims, ok := token.Claims.(jwt.MapClaims); ok && claims["access_levels"] != nil {
-	// 		if accessLevels, ok := claims["access_levels"].(string); ok {
-	// 			// Split the access levels into a slice
-	// 			levels := strings.Split(accessLevels, ",")
-	// 			// Check if the required access level is in the slice
-	// 			for _, level := range levels {
-	// 				if strings.TrimSpace(level) == requiredAccessLevel {
-	// 					// Call the next handler if the access level is found
-	// 					next.ServeHTTP(w, r)
-	// 					return
-	// 				}
-	// 			}
-	// 		}
-	// 	}
-
-	// 	// If the required access level is not found, return an error
-	// 	http.Error(w, fmt.Sprintf("access denied: required access level '%s' not found", requiredAccessLevel), http.StatusForbidden)
-	// })
 }
